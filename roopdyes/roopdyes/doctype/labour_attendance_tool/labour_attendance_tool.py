830c4e954b3c25408da65cb2738a43a1bafb3ccc
finbyz <info@finbyz.com>
2018-11-26 08:52:44 +0000

    Fix labour negative attendance

--- roopdyes/roopdyes/doctype/labour_attendance_tool/labour_attendance_tool.py
+++ roopdyes/roopdyes/doctype/labour_attendance_tool/labour_attendance_tool.py
@@ -4,65 +4,53 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe.utils import cint
 from frappe.model.document import Document
-from frappe import db,get_doc
 
 class LabourAttendanceTool(Document):
-
 	def get_labour_list(self):
-		labour_list = db.sql("""
-			select
-				name, labour_name
-			from	
-				`tabLabour`
-			where
-				status != 'Left'
-		""",as_dict=1)
+		condition = ''
+
+		if self.placed_by:
+			condition += " and placed_by = '%s' " % self.placed_by
+
+			if self.placed_by == "Contractor" and self.contractor:
+				condition += "and supplier = '%s' " % self.contractor
+
+		labour_list = frappe.db.sql("""
+			select name, labour_name, placed_by
+			from `tabLabour`
+			where status != 'Left' {0} """.format(condition) ,as_dict=1)
 		
 		self.set("labour_list",[])
 		
 		for row in labour_list:
 			self.append("labour_list",{
 				"labour": row.name,
-				"labour_name": row.labour_name
+				"labour_name": row.labour_name,
+				"placed_by": row.placed_by
 			})
 
-	def before_submit(self):
-		for row in self.labour_list:
-			labour = get_doc("Labour",row.labour)
-			if row.present == 1:		
-				labour.total_working_days += 1
-			labour.total_extra_hours += row.extra_hours
-			labour.save()
-			db.commit()
-		
-	def on_cancel(self):
-		for row in self.labour_list:
-			labour = get_doc("Labour",row.labour)
-			labour.total_working_days -= 1
-			labour.total_extra_hours -= row.extra_hours
-			labour.save()
-			db.commit()
-			
-			
-			
-			
-			
-		
-	# def before_submit(self):
-		# for row in self.labour_list:
-			# labour = get_doc("Labour",row.labour)
-			# for d in labour.daily_attendance:
-				# frappe.errprint(d.date)
-				# if str(d.date) == str(self.date):
-					# frappe.throw("Record already exist for date %s"%self.date)
-				
-			# labour.append("daily_attendance",{
-				# "date": self.date,
-				# "present": row.present,
-				# "extra_hours": row.extra_hours
-			# })
-			
-			# labour.save()
-			# db.commit()
+	def validate(self):
+		if not self.get('labour_list'):
+			self.get_labour_list()
 
+	def on_submit(self):
+		self.update_labour_attendance()
+
+	def on_cancel(self):
+		self.update_labour_attendance()
+
+	def update_labour_attendance(self):
+		for row in self.labour_list:
+			labour = frappe.get_doc("Labour",row.labour)
+			if row.present:
+				if self._action == "submit":
+					labour.total_working_days += 1
+					labour.total_extra_hours += row.extra_hours
+				elif self._action == 'cancel':
+					labour.total_working_days -= 1
+					labour.total_extra_hours -= row.extra_hours
+			labour.save()
+		else:
+			frappe.db.commit()
