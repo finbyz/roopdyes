361fe0e99b95a2612b67bd7810ee7981c61673ba
FinByz Tech Pvt. Ltd. <info@finbyz.tech>
2025-09-24 15:13:57 +0530

    Update forex_exposure.js
    js filter done

--- roopdyes/roopdyes/report/forex_exposure/forex_exposure.js
+++ roopdyes/roopdyes/report/forex_exposure/forex_exposure.js
@@ -14,12 +14,29 @@ frappe.query_reports["Forex Exposure"] = {
 			]
 		},
 		{
-			"fieldname":"timespan",
-			"label": __("Timespan"),
-			"fieldtype": "Select",
-			"width": "80",
-			"default":"this fiscal year",
-			"options":"\nlast week\nlast month\nlast quarter\nlast 6 months\nlast year\ntoday\nthis week\nthis month\nthis quarter\nthis year\nthis fiscal year\nnext week\nnext month\nnext quarter\nnext 6 months\nnext year"
-		}
+            fieldname: "timespan",
+            label: __("Timespan"),
+            fieldtype: "Select",
+            width: "80",
+            default: "This Fiscal Year",
+            options: [
+                { value: "Today", label: __("Today") },
+                { value: "Last Week", label: __("Last Week") },
+                { value: "Last Month", label: __("Last Month") },
+                { value: "Last Quarter", label: __("Last Quarter") },
+                { value: "Last 6 Months", label: __("Last 6 Months") },
+                { value: "Last Year", label: __("Last Year") },
+                { value: "This Week", label: __("This Week") },
+                { value: "This Month", label: __("This Month") },
+                { value: "This Quarter", label: __("This Quarter") },
+                { value: "This Year", label: __("This Year") },
+                { value: "This Fiscal Year", label: __("This Fiscal Year") },
+                { value: "Next Week", label: __("Next Week") },
+                { value: "Next Month", label: __("Next Month") },
+                { value: "Next Quarter", label: __("Next Quarter") },
+                { value: "Next 6 Months", label: __("Next 6 Months") },
+                { value: "Next Year", label: __("Next Year") }
+            ]
+        }
 	]
 }
